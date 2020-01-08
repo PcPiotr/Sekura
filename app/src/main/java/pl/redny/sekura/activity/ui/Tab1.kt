4e26696c0325649076e5023e82b0792ea2c40904
Piotr Janicki <piotr.janicki@hycom.pl>
2020-01-08 13:55:57 +0100

    optimalization

--- app/src/main/java/pl/redny/sekura/activity/ui/Tab1.kt
+++ app/src/main/java/pl/redny/sekura/activity/ui/Tab1.kt
@@ -1,27 +1,36 @@
 package pl.redny.sekura.activity.ui
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.tab1.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import pl.redny.sekura.R
-import pl.redny.sekura.view.filePicker.FilePicker
 import pl.redny.sekura.encryption.AESEncryptor
 import pl.redny.sekura.encryption.DESEncryptor
 import pl.redny.sekura.encryption.Encryptor
 import pl.redny.sekura.util.ResourcesUtil
+import pl.redny.sekura.view.filePicker.FilePicker
 import java.io.FileNotFoundException
 import java.io.InputStream
 import java.io.OutputStream
+
 
 class Tab1 : Fragment() {
     /**
@@ -29,24 +38,39 @@ class Tab1 : Fragment() {
      */
     private val TAG = "APIChecker"
 
+    val CHANNEL_ID = "SekuraEncryptionChannel"
+
     private val filePicker: FilePicker by inject()
 
     private var encryptor: Encryptor = AESEncryptor()
+
+    private var notificationManager: NotificationManager? = null
+
+    private var notificationBuilder: NotificationCompat.Builder? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        createNotificationChannel()
         return inflater.inflate(R.layout.tab1, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onStart() {
         super.onStart()
         button_picker_1.setOnClickListener { filePicker.openFilePicker(activity!!, 2137) }
         button_picker_2.setOnClickListener { filePicker.openSaveFile(activity!!, "file", 2138) }
         button_encrypt_action.setOnClickListener { onEncryptButton() }
         button_default_action.setOnClickListener { onDefaultButton() }
+        notificationManager = activity!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+        notificationBuilder = NotificationCompat.Builder(activity)
+        notificationBuilder!!.setContentTitle("Encryption")
+            .setSmallIcon(R.drawable.ic_menu_send)
+            .setProgress(100, 0, true)
+            .setChannelId(CHANNEL_ID)
+            .priority = NotificationManager.IMPORTANCE_DEFAULT
     }
 
     private fun toast(stringValue: Int) {
@@ -95,31 +119,58 @@ class Tab1 : Fragment() {
             return
         }
 
-        if (radio_aes.isChecked) {
-            if (password.length < 16) {
-                Log.w(TAG, "Password too short")
-                toast(R.string.encryption_incorrect_password_AES)
+        encryptor = when {
+            radio_aes.isChecked -> {
+                AESEncryptor()
+            }
+            radio_des.isChecked -> {
+                DESEncryptor()
+            }
+            else -> {
+                Log.e(TAG, "Encryptor not found")
                 return
             }
-            encryptor = AESEncryptor()
-        } else if (radio_des.isChecked) {
-            if (password.length < 8) {
-                Log.w(TAG, "Password too short")
-                toast(R.string.encryption_incorrect_password_DES)
-                return
-            }
-            encryptor = DESEncryptor()
-        } else {
-            Log.e(TAG, "Encryptor not found")
-            return
         }
 
-        if (mode) {
-            encryptor.encrypt(password, inputStream!!, outputStream!!)
-        } else {
-            encryptor.decrypt(password, inputStream!!, outputStream!!)
+        try {
+            if (mode) {
+                GlobalScope.launch {
+                    notificationBuilder!!.setContentText("Encryption process in progress...")
+                    notificationManager!!.notify(420, notificationBuilder!!.build())
+                    encryptor.encrypt(password, inputStream!!, outputStream!!)
+                    notificationBuilder!!.setContentText("Encryption completed.")
+                        .setProgress(0, 0, false)
+                    notificationManager!!.notify(420, notificationBuilder!!.build())
+                }
+
+            } else {
+                GlobalScope.launch {
+                    notificationBuilder!!.setContentText("Decryption process in progress...")
+                    notificationManager!!.notify(420, notificationBuilder!!.build())
+                    encryptor.decrypt(password, inputStream!!, outputStream!!)
+                    notificationBuilder!!.setContentText("Decryption completed.")
+                        .setProgress(0, 0, false)
+                    notificationManager!!.notify(420, notificationBuilder!!.build())
+                }
+            }
+        } catch (exception: Encryptor.EncryptorException) {
+            Log.e(TAG, "Error during encryption: " + exception.message)
+            toast(R.string.encryption_unsuccessful)
         }
+
         Log.i(TAG, "Encryption process successful")
         toast(R.string.encryption_successful)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Encryption progess",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            serviceChannel.setShowBadge(false);
+            activity!!.getSystemService(NotificationManager::class.java)!!.createNotificationChannel(serviceChannel)
+        }
     }
 }
--- app/src/main/java/pl/redny/sekura/encryption/AESEncryptor.kt
+++ app/src/main/java/pl/redny/sekura/encryption/AESEncryptor.kt
@@ -4,14 +4,17 @@ import java.io.InputStream
 import java.io.OutputStream
 import javax.crypto.Cipher
 import javax.crypto.CipherOutputStream
+import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.SecretKeySpec
 
 class AESEncryptor : Encryptor {
     private val DEFAULT_READ_WRITE_BLOCK_BUFFER_SIZE = 1024
-    private val ALGO_IMAGE_ENCRYPTOR = "AES/CBC/PKCS5Padding"
+    private val ALGO_IMAGE_ENCRYPTOR = "AES/CBC/PKCS7Padding"
     private val ALGO_SECRET_KEY = "AES"
     private val IV_PARAMETER_SPEC = "C9JkDz3uaEQKpgct"
+    private val SALT = "e9d71f5et7c92d6dc9212ffdad17b8bd4ge18f98".toByteArray(charset("UTF-8"))
 
     override fun encrypt(keyStr: String, input: InputStream, output: OutputStream) {
         logic(keyStr, input, output, true)
@@ -25,13 +28,17 @@ class AESEncryptor : Encryptor {
         var output = output
         try {
             val iv = IvParameterSpec(IV_PARAMETER_SPEC.toByteArray(charset("UTF-8")))
-            val key = SecretKeySpec(keyStr.toByteArray(charset("UTF-8")), ALGO_SECRET_KEY)
+
+            val keySpec = PBEKeySpec("password".toCharArray(), SALT, 65536, 256)
+            val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+            val key: ByteArray = keyFactory.generateSecret(keySpec).getEncoded()
+            val secretKeySpec = SecretKeySpec(key, "AES")
 
             val cipher = Cipher.getInstance(ALGO_IMAGE_ENCRYPTOR)
             if (isEncrypt) {
-                cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+                cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv)
             } else {
-                cipher.init(Cipher.DECRYPT_MODE, key, iv)
+                cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv)
             }
             output = CipherOutputStream(output, cipher)
 
